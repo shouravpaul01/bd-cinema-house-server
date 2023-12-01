c2677ae28e330b18e2ad3b4f105c5ae3dd11c5c9
Shourav paul <paul.shourav01@gmail.com>
2023-12-02 01:41:02 +0600

    vercel

--- api/index.js
+++ api/index.js
@@ -8,11 +8,15 @@ const movie=require('../api/routes/movie')
 const app=express()
 app.use(express.json())
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.8sp76yj.mongodb.net/bdCinemaHouseDB?retryWrites=true&w=majority`).then(()=>console.log('Succesfully connect with server')).catch((err)=>console.log(err))
 
 app.use('/movie',movie)
+
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
 app.listen(3000,(req,res)=>{
     console.log('Port is',3000);
 })
